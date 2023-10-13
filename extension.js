4157b94da571633b3b2029066f78d17fabc6bd40
neo <matheusventureli50@gmail.com>
2023-10-13 11:47:28 -0300

    fix/formatting output when shown event is not from today but yesterday

--- extension.js
+++ extension.js
@@ -3,20 +3,36 @@ const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
 
-let panelButton, panelButtonText;
+let panelButton, panelButtonText, timeout;
 
 function setButtonText () {
-    // date
-    var [ok, out, err, exit] = GLib.spawn_command_line_sync('date "+%Y-%m-%d:"');
-    let today = out.toString().replaceAll("\n", "");
+    const currentDate = new Date();
+    const yesterdayDate = new Date(currentDate);
+    yesterdayDate.setDate(currentDate.getDate() - 1);
+
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const yesterday = String(yesterdayDate.getDate()).padStart(2, '0');
+    
+    const todayFormatted = `${year}-${month}-${day}:`;
+    const yesterdayFormatted = `${year}-${month}-${yesterday}:`;
 
     // calendar
     var [ok, out, err, exit] = GLib.spawn_command_line_sync('cat today.txt');
-    let calendar = out.toString().replaceAll(today, "").replaceAll("\t", "  ");
-    let index_of_last_new_line = calendar.lastIndexOf("\n");
-    let calendar_no_last_new_line = calendar.substring(0, index_of_last_new_line); 
+    let calendarRaw = out.toString();
 
-    panelButtonText.set_text(calendar_no_last_new_line);
+    let calendar_no_full_date = "";
+    if (calendarRaw.includes(todayFormatted)) {
+        let calendar_no_last_new_line = calendarRaw.substring(0, calendarRaw.lastIndexOf("\n")); 
+        calendar_no_full_date = calendar_no_last_new_line.replaceAll(todayFormatted, "").replaceAll("\t", "  ");
+    }
+
+    if (calendarRaw.includes(yesterdayFormatted)) {
+        calendar_no_full_date = calendar_no_full_date.replaceAll(yesterdayFormatted);
+    }
+
+    panelButtonText.set_text(calendar_no_full_date);
 
     return true;
 }
@@ -36,8 +52,10 @@ function init() {
 function enable() {
     setButtonText();
     Main.panel._rightBox.insert_child_at_index(panelButton, 0);
+    timeout = Mainloop.timeout_add_seconds(5.0, setButtonText);   
 }
 
 function disable() {    
+    Mainloop.source_remove(timeout);
     Main.panel._rightBox.remove_child(panelButton);
 }
