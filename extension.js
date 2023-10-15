487f4074faf3cd120e823fb7a250b0052f2f5082
neo <matheusventureli50@gmail.com>
2023-10-14 21:18:12 -0300

    fix/shows events from day before, next day and after tomorrow in better formatting

--- extension.js
+++ extension.js
@@ -8,29 +8,38 @@ let panelButton, panelButtonText, timeout;
 function setButtonText () {
     const currentDate = new Date();
     const yesterdayDate = new Date(currentDate);
+    const tomorrowDate = new Date(currentDate);
+    const dayAfterDate = new Date(currentDate);
+
     yesterdayDate.setDate(currentDate.getDate() - 1);
+    tomorrowDate.setDate(currentDate.getDate() + 1);
+    dayAfterDate.setDate(currentDate.getDate() + 2);
 
     const year = currentDate.getFullYear();
     const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
+    const today = String(currentDate.getDate()).padStart(2, '0');
     const yesterday = String(yesterdayDate.getDate()).padStart(2, '0');
+    const tomorrow = String(tomorrowDate.getDate()).padStart(2, '0');
+    const dayAfter = String(dayAfterDate.getDate()).padStart(2, '0');
     
-    const todayFormatted = `${year}-${month}-${day}:`;
+    const todayFormatted = `${year}-${month}-${today}:`;
     const yesterdayFormatted = `${year}-${month}-${yesterday}:`;
+    const tomorrowFormatted = `${year}-${month}-${tomorrow}:`;
+    const dayAfterFormatted = `${year}-${month}-${dayAfter}:`;
 
     // calendar
     var [ok, out, err, exit] = GLib.spawn_command_line_sync('cat today.txt');
     let calendarRaw = out.toString();
 
     let calendar_no_full_date = "";
-    if (calendarRaw.includes(todayFormatted)) {
-        let calendar_no_last_new_line = calendarRaw.substring(0, calendarRaw.lastIndexOf("\n")); 
-        calendar_no_full_date = calendar_no_last_new_line.replaceAll(todayFormatted, "").replaceAll("\t", "  ");
-    }
+    let calendar_no_last_new_line = calendarRaw.substring(0, calendarRaw.lastIndexOf("\n")); 
 
-    if (calendarRaw.includes(yesterdayFormatted)) {
-        calendar_no_full_date = calendar_no_full_date.replaceAll(yesterdayFormatted);
-    }
+    calendar_no_full_date = calendar_no_last_new_line
+    .replaceAll(todayFormatted, "")
+    .replaceAll(yesterdayFormatted, "Yesterday: ")
+    .replaceAll(tomorrowFormatted, "Tomorrow: ")
+    .replaceAll(dayAfterFormatted, "Day After: ")
+    .replaceAll("\t", "  ");
 
     panelButtonText.set_text(calendar_no_full_date);
 
